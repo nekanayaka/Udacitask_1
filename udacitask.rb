a4ea010a39f529ea8bde1b4cb970d2c404b26421
nekanayaka <nekanayaka@hollandcollege.com>
2016-05-05 20:59:59 +0000

    Changed the list object a global variable.

--- udacitask.rb
+++ udacitask.rb
@@ -1,7 +1,7 @@
 require_relative 'todolist.rb'
 
 # Creates a new todo list
-list = TodoList.new("Julia's Stuff")
+$list = TodoList.new("Julia's Stuff")
 
 # Add four new items
 item1 = Item.new("Do laundry")
@@ -9,28 +9,28 @@ item2 = Item.new("Feed the cat")
 item3 = Item.new("Buy cereal")
 item4 = Item.new("Go dancing!")
 
-list.add_item(item1)
-list.add_item(item2)
-list.add_item(item3)
-list.add_item(item4)
+$list.add_item(item1)
+$list.add_item(item2)
+$list.add_item(item3)
+$list.add_item(item4)
 
 # Print the list
-for item in items
-  puts "#{list.items.index(item) + 1} - #{item.description} \t #{item.completed_status}"
-end
-# def print_items(items)
-#   for item in items
-#     puts "#{list.items.index(item) + 1} - #{item.description} \t #{item.completed_status}"
-#   end
+# for item in list.items
+#   puts "#{list.items.index(item) + 1} - #{item.description} \t #{item.completed_status}"
 # end
+def print_items(items)
+  for item in items
+    puts "#{items.index(item) + 1} - #{item.description} \t #{item.completed_status}"
+  end
+end
 
 # print_items()
 
 # Delete the first item
-list.delete_item(0)
+$list.delete_item(0)
 
 # Print the list
-# print_items(list.items)
+print_items($list.items)
 
 # Delete the second item
 
