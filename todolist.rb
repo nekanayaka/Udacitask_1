c240a03b3a814b15efd64624c75b325112048fd0
Nimna Ekanayaka <nimna@nimna.me>
2016-05-05 17:04:37 -0300

    Added the TodoList and Item classes with appropriate methods.

---
+++ todolist.rb
@@ -0,0 +1,32 @@
+# todolist.rb
+
+class TodoList
+    attr_reader :title, :items
+
+     # Initialize todo list with a title and no items
+     def initialize(list_title)
+        @title = list_title
+        @items = Array.new # Starts empty! No Items yet!
+     end
+
+     # Creates a new Item and adds it to the array of Items
+     def add_item(new_item)
+         item = Item.new(new_item)
+         @items.push(item)
+     end
+
+     def delete_item(item_index)
+       items.delete(items[item_index])
+     end
+end
+
+class Item
+     attr_reader :description, :completed_status
+
+     # Initialize item with a description and marked as
+     # not complete
+     def initialize(item_description)
+        @description = item_description
+        @completed_status = false
+     end
+end
