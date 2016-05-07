29e811f76ee0efba9ae3e79283ac4866adaedd66
nekanayaka <nekanayaka@hollandcollege.com>
2016-05-07 22:42:26 +0000

    Prints the list after each of the requirements.

--- udacitask.rb
+++ udacitask.rb
@@ -19,27 +19,46 @@ $list.add_item(item4)
 #   puts "#{list.items.index(item) + 1} - #{item.description} \t #{item.completed_status}"
 # end
 def print_items(items)
-  for item in items
+  # for item in items
+  #   puts "#{items.index(item) + 1} - #{item.description} \t #{item.completed_status}"
+  # end
+  items.each do |item|
     puts "#{items.index(item) + 1} - #{item.description} \t #{item.completed_status}"
   end
 end
 
-# print_items()
+puts $list.title
+print_items($list.items)
+puts "\n"
 
 # Delete the first item
 $list.delete_item(0)
 
 # Print the list
+puts $list.title
 print_items($list.items)
+puts "\n"
 
 # Delete the second item
+$list.delete_item(1)
 
 # Print the list
+puts $list.title
+print_items($list.items)
+puts "\n"
 
 # Update the completion status of the first item to complete
+$list.items[0].completed_status = true
 
 # Print the list
+puts $list.title
+print_items($list.items)
+puts "\n"
 
 # Update the title of the list
+$list.title = "Julia's new stuff"
 
 # Print the list
+puts $list.title
+print_items($list.items)
+puts "\n"
