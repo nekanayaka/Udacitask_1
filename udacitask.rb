2b9d8c5e677616161ec8cfa3eb1a64f1b03c6675
nekanayaka <nekanayaka@hollandcollege.com>
2016-05-08 00:27:18 +0000

    Removed the print methods from the launcher and items are set to strings.

--- udacitask.rb
+++ udacitask.rb
@@ -1,64 +1,42 @@
 require_relative 'todolist.rb'
 
 # Creates a new todo list
-$list = TodoList.new("Julia's Stuff")
+list = TodoList.new("Julia's Stuff")
 
 # Add four new items
-item1 = Item.new("Do laundry")
-item2 = Item.new("Feed the cat")
-item3 = Item.new("Buy cereal")
-item4 = Item.new("Go dancing!")
+item1 = "Do laundry"
+item2 = "Feed the cat"
+item3 = "Buy cereal"
+item4 = "Go dancing!"
 
-$list.add_item(item1)
-$list.add_item(item2)
-$list.add_item(item3)
-$list.add_item(item4)
+list.add_item(item1)
+list.add_item(item2)
+list.add_item(item3)
+list.add_item(item4)
 
 # Print the list
-# for item in list.items
-#   puts "#{list.items.index(item) + 1} - #{item.description} \t #{item.completed_status}"
-# end
-def print_items(items)
-  # for item in items
-  #   puts "#{items.index(item) + 1} - #{item.description} \t #{item.completed_status}"
-  # end
-  items.each do |item|
-    puts "#{items.index(item) + 1} - #{item.description} \t #{item.completed_status}"
-  end
-end
-
-puts $list.title
-print_items($list.items)
-puts "\n"
+list.print_list
 
 # Delete the first item
-$list.delete_item(0)
+list.delete_item(1)
 
 # Print the list
-puts $list.title
-print_items($list.items)
-puts "\n"
+list.print_list
 
 # Delete the second item
-$list.delete_item(1)
+list.delete_item(2)
 
 # Print the list
-puts $list.title
-print_items($list.items)
-puts "\n"
+list.print_list
 
 # Update the completion status of the first item to complete
-$list.items[0].completed_status = true
+list.items[0].completed_status = true
 
 # Print the list
-puts $list.title
-print_items($list.items)
-puts "\n"
+list.print_list
 
 # Update the title of the list
-$list.title = "Julia's new stuff"
+list.title = "Julia's new stuff"
 
 # Print the list
-puts $list.title
-print_items($list.items)
-puts "\n"
+list.print_list
