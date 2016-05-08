0ddb0118ed6986f0f06f65f3567d150a26860ffa
nekanayaka <nekanayaka@hollandcollege.com>
2016-05-08 00:28:54 +0000

    Added the print method to the TodoList class to print out the details.

--- todolist.rb
+++ todolist.rb
@@ -12,12 +12,20 @@ class TodoList
 
      # Creates a new Item and adds it to the array of Items
      def add_item(new_item)
-         item = Item.new(new_item)
-         @items.push(item)
+        item = Item.new(new_item)
+        @items.push(item)
      end
 
      def delete_item(item_index)
-       items.delete(items[item_index])
+       items.delete(items[item_index - 1])
+     end
+     
+     def print_list
+        puts @title
+        for item in @items
+            puts "#{@items.index(item)} - #{item.description} \t Completed: #{item.completed_status}"
+        end
+        puts "\n"
      end
 end
 
